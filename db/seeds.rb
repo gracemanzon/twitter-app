792d94791f99b1c7ca35cf3a4f10d5e4c47e593c
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-09-19 17:10:51 -0400

    add seeds file with foundational db data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Robyn", email: "robyn@test.com", password_digest: "password")
+
+User.create(name: "Marcus", email: "marcus@test.com", password_digest: "password")
+
+User.create(name: "Grace", email: "grace@test.com", password_digest: "password")
+
+Relationship.create(follower_id: 4, leader_id: 5)
+
+Relationship.create(follower_id: 5, leader_id: 4)
+
+Relationship.create(follower_id: 6, leader_id: 4)
