24cf2217af35013c91967549049bdff171ab5e96
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-09-19 17:20:51 -0400

    add users and sessions controllers with associated routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # users routes
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # sessions routes
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
