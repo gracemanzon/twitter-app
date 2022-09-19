c67bdb93b0a2331de85587e172982a061f3c1eb0
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-09-19 17:16:29 -0400

    add belongs_to/has_many relations and validations for relationship and user models

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ User.create(name: "Marcus", email: "marcus@test.com", password_digest: "password
 
 User.create(name: "Grace", email: "grace@test.com", password_digest: "password")
 
-Relationship.create(follower_id: 4, leader_id: 5)
+Relationship.create(follower_id: 1, leader_id: 2)
 
-Relationship.create(follower_id: 5, leader_id: 4)
+Relationship.create(follower_id: 2, leader_id: 1)
 
-Relationship.create(follower_id: 6, leader_id: 4)
+Relationship.create(follower_id: 3, leader_id: 1)
