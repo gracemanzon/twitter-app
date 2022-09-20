9cf8404e5d02cdf32c211f7dac4b622519636557
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-09-20 14:41:58 -0400

    add user profile show action and edit/update validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   has_many :followers, through: :follower_relationships, source: :follower
   has_many :leader_relationships, class_name: "Relationship", foreign_key: "follower_id"
   has_many :leaders, through: :leader_relationships, source: :leader
+  has_many :tweets
 end
