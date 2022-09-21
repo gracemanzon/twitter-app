61334d7ab9c6f14c167f84b1224abd1ecc626e70
RobynSpaulding <robynm27@gmail.com>
2022-09-21 10:24:02 -0700

    adds destory method to relationships controller and routes, adds unfollow button

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,5 +4,11 @@ class RelationshipsController < ApplicationController
     Relationship.create(follower_id: current_user.id, leader_id: params[:leader_id])
   end
 
+  def destroy
+    relationships = Relationship.where(follower_id: current_user.id, leader_id: params[:leader_id])
+    relationships.destroy_all
+    render json: {message: "User successfully unfollowed"}
+  end
+
 end
 
