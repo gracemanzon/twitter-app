d726ecbb14df492614a4de165ce2d7d8895f05d5
Marcus Davis <marcusdavis1018@gmail.com>
2022-09-23 15:56:18 -0400

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,13 @@ User.create(name: "Marcus", email: "marcus@test.com", password: "password", imag
 
 User.create(name: "Grace", email: "grace@test.com", password: "password", image_url: "https://thewellesleynews.com/wp-content/uploads/2020/09/avatar.jpg")
 
+
+User.create(name: "Kevyn", email: "kevyn@test.com", password: "password", image_url: "https://qph.cf2.quoracdn.net/main-qimg-8f6df1af9a46d5b2e81e6eaf0543d86a-lq")
+
+User.create(name: "Greg", email: "greg@test.com", password: "password", image_url: "https://pbs.twimg.com/profile_images/3541863016/6ac8c451e0e45a13050ed5f3f1dd25ee_400x400.png")
+
+User.create(name: "Joey", email: "joey@test.com", password: "password", image_url: "https://www.looper.com/img/gallery/netflixs-avatar-the-last-airbender-has-finally-found-its-fire-lord-ozai/l-intro-1635984181.jpg")
+
 Relationship.create(follower_id: 1, leader_id: 2)
 
 Relationship.create(follower_id: 2, leader_id: 1)
@@ -12,24 +19,70 @@ Relationship.create(follower_id: 3, leader_id: 1)
 
 Tweet.create(
   user_id: 1,
-  title: "tweet title by robyn",
-  body: "Hello World, it's Robyn!",
-  image_url: "https://thumbs.dreamstime.com/b/bee-flower-27533578.jpg",
+  title: "Site is up",
+  body: "Hello World, it's Robyn. I helped make this app!",
+  image_url: "https://preview.redd.it/5yhx7zsi7ug81.jpg?width=640&crop=smart&auto=webp&s=af94ec8e098d27f9b2ee3160c0e889674a3291e5",
 )
 
 Tweet.create(
   user_id: 2,
-  title: "tweet title by marcus",
-  body: "Hello World, it's Marcus!",
-  image_url: "https://thumbs.dreamstime.com/b/bee-collecting-pollen-2250640.jpg",
+  title: "Suggestions",
+  body: "Hello World, it's Marcus. Got any suggestions on how to improve our site? Comment below",
+  image_url: "https://codingbootcamps.io/wp-content/uploads/m2.png",
 )
 
 Tweet.create(
   user_id: 3,
-  title: "tweet title by grace",
-  body: "Hello World, it's Grace!",
-  image_url: "https://thumbs.dreamstime.com/b/landscape-nature-mountan-alps-rainbow-76824355.jpg",
+  title: "Thoughts",
+  body: "This website is banging! I think we made Joey proud.",
+  image_url: "https://ichef.bbci.co.uk/images/ic/1920x1080/p05lqc4f.jpg",
 )
+
+Tweet.create(
+  user_id: 4,
+  title: "Feeling blue",
+  body: "Day 3, still haven't recovered from covid smh :(",
+)
+
+Tweet.create(
+  user_id: 5,
+  title: "Meme of the day",
+  body: "lol",
+  image_url: "https://preview.redd.it/1gpqmbfm7um91.gif?width=640&format=mp4&s=b5533ec9d5ad285f0f1139d5ad5299fa5d4d1322",
+)
+
+Tweet.create(
+  user_id: 6,
+  title: "THIS SUCKS?",
+  body: "I am deleting my account",
+  image_url: "https://c.tenor.com/hgYm7BcnSx0AAAAC/lame.gif",
+)
+
+
+Tweet.create(
+  user_id: 2,
+  title: "so true",
+  body: "enjoy the memes",
+  image_url: "https://preview.redd.it/bg0xjiz5u9k91.gif?width=640&format=mp4&s=8400e5a502fafac6b73e44b1f2ac1c30e4818451",
+)
+
+
+Tweet.create(
+  user_id: 1,
+  title: "haha",
+  body: "wow, so relatable",
+  image_url: "https://preview.redd.it/vgo85g9ozfj91.jpg?width=640&crop=smart&auto=webp&s=cbaf529e2c67f98258c63af8652028d1eb9853e2",
+)
+
+Tweet.create(
+  user_id: 2,
+  title: "rofl",
+  body: "probably what rlly happened lol",
+  image_url: "https://preview.redd.it/gph4rp6drvo41.jpg?width=640&crop=smart&auto=webp&s=a407a7be1da73ba010f0295a6351ab9d14471b2a",
+)
+
+
+
 
 # 10.times do
 #   User.create(
